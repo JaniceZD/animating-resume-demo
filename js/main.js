703fb8d10ec479fecedaab2cfbf829d011a145dc
JaniceZD <mnzd123@gmail.com>
2023-07-08 23:20:07 +0800

    update

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ function writeCode(prefix, code, fn) {
       window.clearInterval(id)
       fn && fn.call()
     }
-  }, 23)
+  }, 20)
 }
 function writeMarkdown(markdown, fn) {
   let domPaper = document.querySelector('#paper > .content')
@@ -67,14 +67,14 @@ html{
   position: fixed; left: 0; top: 0;
   -webkit-transition: none;
   transition: none;
-  -webkit-transform: rotateY(10deg) translateZ(-100px);
-  transform: rotateY(10deg) translateZ(-100px);
+  -webkit-transform: rotateY(10deg) translateZ(20px);
+  transform: rotateY(10deg) translateZ(20px);
 }
 
 /* 现在正式开始 */
 /* 我需要一张白纸 */
 #paper{
-  position: fixed; right: 0; top: 0;
+  position: fixed; right: .5em; top: 0;
 }
 #paper>.content{
   border: 1px solid;
@@ -92,53 +92,57 @@ var css2 = `
 `
 var md = `
 # 张丹  
-女 | 25岁 | 前端开发工程师 | 深圳
+女 | 28岁 | 前端开发工程师 | <strong>2年经验</strong> | 深圳
 
-[我的博客](https://www.jianshu.com/u/56c6b2bd891d)，目前已有 <strong>45</strong> 篇技术文章
+[我的博客](https://www.jianshu.com/u/56c6b2bd891d)，目前已有 <strong>50</strong> 篇技术文章
 
 [GitHub](https://github.com/JaniceZD)
 
 # 技能介绍  
 * 熟悉页面制作技巧，能编写具有 <strong>语义化</strong> 的页面，并将设计稿 <strong>完美还原</strong>
+* 熟悉 Sass、Less 等 CSS 预处理库
 * 熟悉 <strong>前后端分离</strong> 技术，包括 AJAX、跨域、前端路由、Cookie、Session 等
-* 熟练掌握 <strong>Vue全家桶</strong> 的使用，包括 VueCli、VueRouter、Vuex 等
-* 熟练掌握 <strong>React</strong>、Redux、React Router 等
+* 熟练掌握 <strong>Vue全家桶</strong> 的使用，包括 Vue3、VueRouter、Vuex 等
+* 熟练掌握 React、Redux、React Router 等
 * 熟悉 <strong>ES6</strong>，包括 let / Promise / await / 析构赋值
 * 了解 <strong>TypeScript</strong> 的使用，了解 <strong>Webpack 的配置和优化</strong>
 * CET-4/6，能够阅读 <strong>英文</strong> 文档，善用Google，Stack Overflow
 
 # 项目介绍
 <header class="header">
-  <h3>柠檬 UI（一款基于Vue的前端UI组件库）</h3>
+  <h3>柠檬 UI（一款基于 Vue3 的 UI 组件库）</h3>
   <span>
-    <a href="https://github.com/JaniceZD/gulu-demo" target="_blank">源码链接</a>
-    <a href="https://janicezd.gitee.io/gulu-demo" target="_blank">项目预览</a>
+    <a href="https://github.com/JaniceZD/gulu-ui-vue3" target="_blank">源码链接</a>
+    <a href="https://janicezd.gitee.io/gulu-ui-vue3" target="_blank">项目预览</a>
   </span>
 </header>
-该项目是参考 <strong>Element UI</strong>、<strong>Ant Design</strong> 等UI库思路制作的 UI 组件库。
-目前已支持包括按钮、Tabs标签页、Popover弹出框、Collapse折叠面板等多个常用组件。
-并使用 VuePress 制作 <strong>官方文档</strong>，发布于npmjs.org。
+该项目是一款基于 <strong>Vue 3</strong>、<strong>TypeScript</strong>、<strong>Vite</strong> 开发的 UI 组件库。<br>
+组件库内包含各种样式的按钮、Switch 开关、Dialog 对话框、Tabs 标签页等常用组件。
 
 <header>
-  <h3>番茄土豆闹钟</h3>
+  <h3>大屏数据可视化项目</h3>
   <span>
-    <a href="https://github.com/JaniceZD/potato-demo-test" target="_blank">源码链接</a>
-    <a href="https://janicezd.gitee.io/potato-demo-test" target="_blank">项目预览</a>
+    <a href="https://github.com/JaniceZD/big-screen-project" target="_blank">源码链接</a>
+    <a href="https://janicezd.gitee.io/big-screen-project" target="_blank">项目预览</a>
   </span>
 </header>
-该项目使用 <strong>React、Redux、React-Router</strong> 等实现了仿番茄土豆的线上应用。
-提供登录注册、番茄闹钟计时、历史任务查看、数据统计等功能。
+该项目采用 <strong>React</strong> 和 <strong>React Router</strong> 构建页面，使用 <strong>flex 布局</strong> 完成页面展示。<br>
+使用 <strong>ECharts</strong> 实现柱状图、折线图、饼图等图表数据的展示，并能实时更新数据。
 
 <header>
-  <h3>小程序：番茄 TodoLsit</h3>
-  <span>
-    <a href="https://github.com/JaniceZD/tomato_mini_demo" target="_blank">源码链接</a>
-  </span>
+  <h3>睿远基金APP</h3>
 </header>
-一款根据 <strong>番茄工作法</strong> 概念而进行开发的 <strong>微信小程序</strong>。
-提供登录注册、历史Todo查看、完成任务记录、番茄钟计时以及番茄钟暂停再来一组的功能。
+该项目是基于 <strong>Vue</strong> 和 <strong>TypeScript</strong> 开发的一款金融理财应用。<br>
+主要负责开发和优化专注森林、资讯、定投交易等模块，实现了业务功能和交互效果。
 
 <header>
+  <h3>睿远基金中台管理系统</h3>
+</header>
+这是一个使用 <strong>Vue</strong>、<strong>VueRouter</strong>、<strong>ElementUI</strong> 等技术开发的管理系统。<br>
+主要负责基金配置模块，独立开发资讯管理模块。
+
+# 开源项目
+<header class="header">
   <h3>动态画出皮卡丘</h3>
   <span>
     <a href="https://github.com/JaniceZD/pikachu-demo" target="_blank">源码链接</a>
@@ -147,17 +151,6 @@ var md = `
 </header>
 这是一个基于原生 <strong>JavaScript</strong> 和 <strong>CSS3</strong> 完成的项目。
 利用 <strong>JS动态写入HTML和CSS</strong>，模拟从零到一画出皮卡丘的过程。
-
-# 开源项目
-<header class="header">
-  <h3>vue技术栈实现cnode社区</h3>
-  <span>
-    <a href="https://github.com/JaniceZD/cnode-demo" target="_blank">源码链接</a>
-    <a href="https://janicezd.gitee.io/cnode-demo" target="_blank">项目预览</a>
-  </span>
-</header>
-这是一个基于 <strong>Vue</strong> 实现的仿 CNode 社区网站。
-主要功能包括帖子查看、用户信息查询、不同主题切换等。
 
 <header>
   <h3>Canvas在线画板</h3>
@@ -175,6 +168,16 @@ var md = `
 
 # 工作经历
 <header class="header">
+  <h3>深圳市进汇科技有限公司</h3>
+  <h4>前端开发工程师</h4>
+  <span><time>2020年12月</time>~<time>2023年2月</time></span>
+</header>
+主要负责公司金融产品的前端研发工作；<br>
+参与前端组件库、常⽤⼯具的搭建，持续提⾼团队的⽣产效率；<br>
+负责团队技术文档的编写与维护，建立常见问题记录等实用专题；<br>
+定期组织内部分享，包括Git的使用、ES6新特性、Vue3等话题。
+
+<header>
   <h3>惠州睿通文化传播有限公司</h3>
   <h4>数据分析师</h4>
   <span><time>2018年10月</time>~<time>2020年8月</time></span>
